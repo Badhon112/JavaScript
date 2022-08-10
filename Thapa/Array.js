a049ae0d209bb972ea3f3b41182c59cbceb1ae01
Badhon <biswasbadhon896@gmail.com>
2022-08-11 02:01:06 +0600

    Array

---
+++ Thapa/Array.js
@@ -0,0 +1,23 @@
+// Traversal of an Array📌
+// Array And Loop
+// var friends=["Badhon","Biswas","Rohim","Karim"];
+//For In Loop show index number
+// for(let friend in friends){
+//     console.log(friend);
+// }
+
+// For of Loop show data in the Array
+
+// for(let friend of friends){
+//     console.log(friend);
+// }
+
+//For Each 
+// friends.forEach((element,index,Array)=>{
+//     console.log("Element is "+element+" Index in: " + index+"Array is: "+Array);
+// })
+
+//Searching and Filter in An Array✔🐱‍🚀📌
+// var friends=["Badhon","Biswas","Rohim","Karim"];
+
+
