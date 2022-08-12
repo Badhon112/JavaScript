2cf94a89922ad2c1bf0c1c3d19bd55c2bef8ea8c
Badhon <biswasbadhon896@gmail.com>
2022-08-12 22:52:55 +0600

    String

---
+++ Thapa/String.js
@@ -0,0 +1,21 @@
+// String Javascript 🕉🕉
+
+// console.log('Hello "Badhon"');
+
+// Finding a String in a String 📌 Searching
+// const data ="My name is Badhon Biswas"
+// console.log(data.indexOf("name"));
+// console.log(data.search("name"));
+// Extracting String Parts 3 methods slice(start,end) substring(start,end) substr(start,length)
+// let str="Badhon Emran Karim"
+// let newstr=str.slice(7,str.length)
+// console.log(newstr);
+
+// Replace in String
+// let data="I am Badhon Biswas"
+// console.log(data.replace("am","Am"));
+
+//Char At()
+let str="BADHON BISWAS"
+// console.log(str.charAt(0));
+console.log(str.charCodeAt(0));
