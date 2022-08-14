5f72dafd2ec28b087658f4edbb723e409170f35e
Badhon <biswasbadhon896@gmail.com>
2022-08-14 20:25:51 +0600

    Date

---
+++ Thapa/Data.js
@@ -0,0 +1,33 @@
+//Get and Set Method
+// let currdate=new Date()
+// console.log(new Date().toLocaleTimeString());
+
+// Date and Time
+
+// Dates Method
+// const currdate = new Date()
+
+// //Get indivisual Date 
+// console.log(currdate.toLocaleDateString());
+// console.log(currdate.getFullYear());
+// console.log(currdate.getMonth());
+// console.log(currdate.getDate());
+// console.log(currdate.getDay());
+//Time Methods
+// const currdate=new Date();
+// console.log(currdate.getTime());
+// const houre=currdate.getHours();
+// if(houre>12){
+//     console.log(houre-12+" PM");
+// }
+// else{
+//     console.log(houre);
+// }
+// change time
+
+(function (){
+    // let da=new Date()
+setTimeout(()=>{
+console.log(new Date.toLocaleDateString());
+},1000)
+})
