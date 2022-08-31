525c68a2864de54a85337856e0a24b7d1e2ef7e1
Badhon <biswasbadhon896@gmail.com>
2022-08-31 18:37:26 +0600

    Object

---
+++ Thapa/Object/Index.js
@@ -0,0 +1,9 @@
+// let mydata={
+//     Myname:"Badhon",
+//     Myage:20,
+//     getname:function(){
+//         console.log(`My name is ${mydata.Myname} and Age ${mydata.Myage}`);
+//     }
+// }
+// mydata.getname();
+console.log(this);
